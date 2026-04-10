98c221475d87e4816a250dbd3b6a91dc60592823
tony.h <bukizvraiki@gmail.com>
2026-04-10 11:00:03 +0200

    Created the Interaction entity with its repository

---
+++ api/src/Entity/Interaction.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InteractionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InteractionRepository::class)]
+class Interaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?bool $isLiked = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $mark = null;
+
+    #[ORM\Column(length: 1000, nullable: true)]
+    private ?string $comment = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isLiked(): ?bool
+    {
+        return $this->isLiked;
+    }
+
+    public function setIsLiked(bool $isLiked): static
+    {
+        $this->isLiked = $isLiked;
+
+        return $this;
+    }
+
+    public function getMark(): ?float
+    {
+        return $this->mark;
+    }
+
+    public function setMark(?float $mark): static
+    {
+        $this->mark = $mark;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): static
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTime $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
