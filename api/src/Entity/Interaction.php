805b78691135087e15d2025c6732022eb9d7a025
tony.h <bukizvraiki@gmail.com>
2026-04-13 12:10:43 +0200

    feat: api added 'interaction:item:read' and 'interaction:collection:read' serialization groups

--- api/src/Entity/Interaction.php
+++ api/src/Entity/Interaction.php
@@ -3,37 +3,56 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\InteractionRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(
+            normalizationContext: ['groups' => ['interaction:item:read']]
+        ),
+        new GetCollection(
+            normalizationContext: ['groups' => ['interaction:collection:read']]
+        ),
+    ],
+    normalizationContext: ['groups' => ['interaction:item:read']]
+)]
 #[ORM\Entity(repositoryClass: InteractionRepository::class)]
 class Interaction
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['interaction:item:read', 'interaction:collection:read'])]
     private ?int $id = null;
 
     #[ORM\Column]
+    #[Groups(['interaction:item:read', 'interaction:collection:read'])]
     private ?bool $isLiked = null;
 
     #[ORM\Column(nullable: true)]
+    #[Groups(['interaction:item:read', 'interaction:collection:read'])]
     private ?float $rate = null;
 
     #[ORM\Column(length: 1000, nullable: true)]
+    #[Groups(['interaction:item:read'])]
     private ?string $comment = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Groups(['interaction:item:read', 'interaction:collection:read'])]
     private ?\DateTime $date = null;
 
     #[ORM\ManyToOne(inversedBy: 'interactions')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['interaction:item:read', 'interaction:collection:read'])]
     private ?User $associatedUser = null;
 
     #[ORM\ManyToOne(inversedBy: 'interactions')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['interaction:item:read', 'interaction:collection:read'])]
     private ?Content $associatedContent = null;
 
     public function getId(): ?int
