674787d81950f72ee31a6626fbdf6ee8e4927568
tony.h <bukizvraiki@gmail.com>
2026-04-13 16:23:31 +0200

    installed orm-fixtures package

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
