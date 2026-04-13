967d8b0cf6ac7c7d4048eac7a9badb4951b96c51
tony.h <bukizvraiki@gmail.com>
2026-04-13 17:21:04 +0200

    feat: loaded 5 Content rows

---
+++ api/src/DataFixtures/ContentFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\ContentFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ContentFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        ContentFactory::createMany(5);
+
+        $manager->flush();
+    }
+}
