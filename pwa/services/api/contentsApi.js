c3b5b4ecf6ac53a9899bb43aa96abcbe026b0842
tony.h <bukizvraiki@gmail.com>
2026-04-22 11:21:42 +0200

    feat: added the getContentById function and renamed getMovies() to getContents()

--- pwa/services/api/contentsApi.js
+++ pwa/services/api/contentsApi.js
@@ -1,15 +1,30 @@
 export const API_BASE_URL = "https://localhost";
 
-export async function getMovies() {
+export async function getContents() {
   try {
     const response = await fetch(`${API_BASE_URL}/contents`);
 
     if (!response.ok) {
-      throw new Error(`An error occured while fetching contents: HTTP ${res.status}`);
+      throw new Error(`An error occured while fetching contents: HTTP ${response.status}`);
     }
     const data = await response.json();
 
     return data["member"];
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getContentById(id) {
+  try {
+    const response = await fetch(`${API_BASE_URL}/contents/${id}`);
+
+    if (!response.ok || response.status === 404) {
+      throw new Error(`An error occured while fetching the content or the content doesn't exist: HTTP ${response.status}`);
+    }
+    const data = await response.json();
+
+    return data;
   } catch (error) {
     throw error;
   }
