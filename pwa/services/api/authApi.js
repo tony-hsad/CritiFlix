8e95e28bb8a1d0538b03c036327bcdb78b9bcfdb
tony.h <bukizvraiki@gmail.com>
2026-04-24 12:33:29 +0200

    feat: added the login, logout and getMe feature with the User's JWT authentication

---
+++ pwa/services/api/authApi.js
@@ -0,0 +1,59 @@
+export const API_BASE_URL = "https://localhost";
+
+export function login(email, password) {
+  return fetch(`${API_BASE_URL}/api/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        // throw response.status;
+        throw new Error("Email ou mot de passe incorrect");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      localStorage.setItem("jwt_token", data.token);
+      return data.token;
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+export function logout() {
+  localStorage.removeItem("jwt_token");
+}
+
+export function getMe() {
+  const token = localStorage.getItem("jwt_token");
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+  return fetch(`${API_BASE_URL}/api/me`, {
+    method: "GET",
+    headers,
+  })
+    .then((response) => {
+      const isUserUnauthorized = response.status === 401;
+      if (isUserUnauthorized) {
+        localStorage.removeItem("jwt_token");
+      }
+
+      if (!response.ok) {
+        throw response.status;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
