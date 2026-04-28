9ad5a7a02f28f9ef3bbf389f22401baf05d2baa4
tony.h <bukizvraiki@gmail.com>
2026-04-28 16:14:08 +0200

    feat: created the login page that calls the LoginForm component

---
+++ pwa/pages/login.js
@@ -0,0 +1,9 @@
+import LoginForm from "../components/organisms/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-950 px-4">
+      <LoginForm />
+    </div>
+  );
+}
