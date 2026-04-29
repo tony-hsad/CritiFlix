e7463c3ce3ec8e78b97f710451e8741c3a2f377b
tony.h <bukizvraiki@gmail.com>
2026-04-29 13:29:29 +0200

    feat: created the RegisterForm organism to display the register form

---
+++ pwa/pages/register.js
@@ -0,0 +1,9 @@
+import RegisterForm from "../components/organisms/RegisterForm";
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-950 px-4">
+      <RegisterForm />
+    </div>
+  );
+}
