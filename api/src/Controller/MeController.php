a42643bd279a922e1df6f86c3c296e1826f307ef
tony.h <bukizvraiki@gmail.com>
2026-05-05 12:42:54 +0200

    edit: MeController uses now the built-in serializer

--- api/src/Controller/MeController.php
+++ api/src/Controller/MeController.php
@@ -5,27 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class MeController extends AbstractController
 {
     #[Route('/api/me', name: 'api_me', methods: ['GET'])]
-    public function me(): JsonResponse
+    public function me(SerializerInterface $serializer): JsonResponse
     {
         $user = $this->getUser();
         if (!$user) {
             return $this->json(['error' => '401 Unauthorized (you are not authenticated)'], 401);
         }
 
-        return $this->json([
-            'id' => $user->getId(),
-            'email' => $user->getUserIdentifier(),
-            'interactions' => $user->getInteractions(),
-            'friends' => $user->getFriends(),
-            'firstname' => $user->getFirstname(),
-            'lastname' => $user->getLastname(),
-            'dateOfBirth' => $user->getDateOfBirth(),
-            'createdAt' => $user->getCreatedAt(),
-            'roles' => $user->getRoles(),
-        ]);
+        $jsonContent = $serializer->serialize(
+            $user,
+            'json',
+            ['groups' => ['user:me', 'user:read', 'user:item:read']],
+        );
+
+        return JsonResponse::fromJsonString($jsonContent);
     }
 }
