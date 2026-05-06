6fdf84848473f5b961a4b5daee2dc5435b661837
tony.h <bukizvraiki@gmail.com>
2026-05-06 10:58:31 +0200

    Resolved conflicts

--- pwa/pages/login.js
+++
@@ -1,9 +0,0 @@
-import LoginForm from "../components/organisms/LoginForm";
-
-export default function LoginPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-950 px-4">
-      <LoginForm />
-    </div>
-  );
-}
