eb4c067aac022a2de7b3d8a8c496013e61803b15
tony.h <bukizvraiki@gmail.com>
2026-05-13 13:04:41 +0200

    edit: added getMorePaginationButtons() function to see more pagination pages

--- pwa/components/molecules/Pagination.tsx
+++ pwa/components/molecules/Pagination.tsx
@@ -1,15 +1,25 @@
 import Button from "../atoms/Button";
 import { ArrowLeftToLine, ArrowRightFromLine } from "lucide-react";
-import type { PaginationProps } from "@/types/Pagination";
+import type { PaginationProps, PaginationType} from "@/types/Pagination";
 
 function Pagination({ pagination, onChangePage }: PaginationProps) {
-  const pages = [];
-  for (let p = pagination.current - 2; p <= pagination.current + 2; p++) {
-    if (p > pagination.first && p < pagination.last) {
-      pages.push(p);
+  function getMorePaginationButtons(pagination: PaginationType) {
+    const additionalPagesAmount = 2;
+    const leftCurrent = pagination.current - additionalPagesAmount;
+    const rightCurrent = pagination.current + additionalPagesAmount;
+
+    const pages = [];
+    for (let p = leftCurrent; p <= rightCurrent; p++) {
+      const isPageBetweenBoundaries = p > pagination.first && p < pagination.last;
+      if (isPageBetweenBoundaries) {
+        pages.push(p);
+      }
     }
+
+    return pages;
   }
 
+  const pages = getMorePaginationButtons(pagination);
   return (
     <div className="mt-8 flex items-center justify-center gap-2">
       <Button
