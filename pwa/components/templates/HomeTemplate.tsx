79d1517a0ae441579cb2e5ef7e438a10c50bc288
tony.h <bukizvraiki@gmail.com>
2026-05-18 12:41:54 +0200

    fix: fixed the white area when resizing the mobile viewport

--- pwa/components/templates/HomeTemplate.tsx
+++ pwa/components/templates/HomeTemplate.tsx
@@ -8,7 +8,7 @@ type HomeTemplateProps = {
 
 function HomeTemplate({ children }: HomeTemplateProps) {
   return (
-    <div className="flex min-h-screen flex-col bg-black text-white">
+    <div className="flex min-h-screen flex-col overflow-x-hidden bg-black text-white">
       <Navbar />
       <main className="flex-1 mx-auto w-full max-w-7xl px-6 py-8">
         {children}
