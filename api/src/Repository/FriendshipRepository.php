1f1b333d78cc1c433a5f0c6bdef5ebc468eee6da
tony.h <bukizvraiki@gmail.com>
2026-05-20 11:11:44 +0200

    feat: added findAlreadyExistingFriendship() query to get friend requests

--- api/src/Repository/FriendshipRepository.php
+++ api/src/Repository/FriendshipRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Friendship;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,6 +15,17 @@ class FriendshipRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Friendship::class);
+    }
+
+    public function findAlreadyExistingFriendship(User $user1, User $user2): ?Friendship
+    {
+        return $this->createQueryBuilder('f')
+            ->where('f.sender = :user1 AND f.receiver = :user2')
+            ->orWhere('f.sender = :user2 AND f.receiver = :user1')
+            ->setParameter('user1', $user1)
+            ->setParameter('user2', $user2)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 
 //    /**
