f6b2fc9b356bfb28ab3bac49252f2adb8eb22fe8
tony.h <bukizvraiki@gmail.com>
2026-05-21 16:25:43 +0200

    feat: added setFriendRequest() function to either accept or reject the request depending on the boolean parameter

--- pwa/services/api/friendshipsApi.ts
+++ pwa/services/api/friendshipsApi.ts
@@ -33,3 +33,28 @@ export function sendFriendRequest(userReceiverId: number): Promise<Friendship> {
       throw error;
     });
 }
+
+export function setFriendRequest(friendshipId:number, isAccept: boolean): Promise<Friendship> {
+  const token = localStorage.getItem("jwt_token");
+  const headers: HeadersInit = {
+    "Content-Type": "application/merge-patch+json",
+  };
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+  return fetch(`${API_BASE_URL}${ROUTES_API.FRIENDSHIPS}/${friendshipId}`, {
+    method: "PATCH",
+    headers,
+    body: JSON.stringify({status: isAccept ? "accepted": "rejected"})
+  })
+  .then((response)=> {
+    if (!response.ok) {
+      throw response.status;
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    throw error;
+  });
+}
