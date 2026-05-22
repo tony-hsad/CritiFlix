6952ed74a19685faeaa847f72d6170eeb5bc9c24
tony.h <bukizvraiki@gmail.com>
2026-05-22 16:03:36 +0200

    edit: added 'user:read' groupes on the 'sender' and 'receiver' fields to access their properties and edited the frontend Friendship type

--- pwa/types/FriendshipsApi.ts
+++ pwa/types/FriendshipsApi.ts
@@ -1,11 +1,12 @@
 import type { ApiResourceBase, Collection } from "@/types/Api";
+import {User} from "@/types/UsersApi";
 
 type FriendshipStatusEnum = 'pending' | 'accepted' | 'rejected';
 
 type FriendshipBase = ApiResourceBase & {
   id: number;
-  sender: string;
-  receiver: string;
+  sender: User;
+  receiver: User;
   status: FriendshipStatusEnum;
   requestDate: string;
   acceptedAt?: string | null;
