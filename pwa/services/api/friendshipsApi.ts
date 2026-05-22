5be7c1736fbb1a9aa4823c3ec07089dec2e63e1c
tony.h <bukizvraiki@gmail.com>
2026-05-22 11:46:47 +0200

    feat: added filters parameters in the Friendship Collection and created the getSentFriendRequests function

--- pwa/services/api/friendshipsApi.ts
+++ pwa/services/api/friendshipsApi.ts
@@ -58,3 +58,29 @@ export function setFriendRequest(friendshipId:number, isAccept: boolean): Promis
     throw error;
   });
 }
+
+export function getSentFriendRequests(userId: number, urlParameters: URLSearchParams): Promise<Friendship> {
+  const token = localStorage.getItem("jwt_token");
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+  };
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+  return fetch(`${API_BASE_URL}${ROUTES_API.FRIENDSHIPS}?sender_id=${userId}&status=pending&${urlParameters.toString()}`,  {
+    headers
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw response.status;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
