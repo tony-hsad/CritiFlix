e5395fa50eae18449da5dc1adc30cdee52ea03e2
tony.h <bukizvraiki@gmail.com>
2026-06-01 14:02:34 +0200

    edit: refactored usersApi functions

--- pwa/services/api/usersApi.ts
+++ pwa/services/api/usersApi.ts
@@ -1,42 +1,11 @@
-import { ROUTES_API } from "../../routes/routes";
-import type { User, UsersCollection } from "@/types/UsersApi";
+import { UserClient } from "./client";
+import type { User, UsersCollection } from "@/types/UsersApi"
+import type { APIPlatformListResponse } from "./client"
 
-export const API_BASE_URL = "https://localhost";
-
-export function getUsers(urlParameters: URLSearchParams): Promise<UsersCollection> {
-  const headers: HeadersInit = {
-    "Content-Type": "application/json",
-  };
-  return fetch(`${API_BASE_URL}${ROUTES_API.USERS}?${urlParameters.toString()}`, {
-    method: "GET",
-    headers,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw response.status;
-      }
-      return response.json();
-    })
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => {
-      throw error;
-    });
+export function getUsers(urlParameters: URLSearchParams): Promise<APIPlatformListResponse<UsersCollection>> {
+  return new UserClient().getList(urlParameters);
 }
 
-export function getUserById(id: number): Promise<User> {
-  return fetch(`${API_BASE_URL}${ROUTES_API.USERS}/${id}`)
-    .then((response) => {
-      if (!response.ok || response.status === 404) {
-        throw response.status;
-      }
-      return response.json();
-    })
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => {
-      throw error;
-    });
+export function getUserById(id: number): Promise<APIPlatformListResponse<User>> {
+  return new UserClient().getItem(id);
 }
