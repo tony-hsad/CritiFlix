627a73a4f16e6ddf634024855befb1f6b5c6ee3b
tony.h <bukizvraiki@gmail.com>
2026-06-06 16:10:36 +0200

    fix: fixed the onChange handle error

--- pwa/components/atoms/Input.tsx
+++ pwa/components/atoms/Input.tsx
@@ -1,7 +1,14 @@
-import React, {ChangeEventHandler} from "react";
+import React from "react";
 
-function Input({ onChange, type = "text", defaultValue, ...rest }: React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...rest} />;
+function Input({ onChange, type = "text", defaultValue, ...rest}: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      onChange={onChange}
+      type={type}
+      defaultValue={defaultValue}
+      {...rest}
+    />
+  );
 }
 
 export default Input;
